57e2913fcb9957b1b9124650a6aa8d180a3ea282
soufian200 <soufianxlm@gmail.com>
2024-03-30 22:23:39 +0000

    add Hero section

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -6,11 +6,26 @@ import Navbar from "./Navbar";
 
 export default function Hero() {
   return (
-    <div className="bg-white">
+    <div className="">
       <Navbar />
 
-      <div className="pt-12">
-        <div className="bg-gray-200">hero</div>
+      <div className="">
+        <div className="bg-cover bg-center h-screen flex  items-center justify-center bg-slate-800 hero">
+          <div className="text-center">
+            <h1 className="text-7xl  font-bold mb-4 text-white">
+              Welcome to ImmoLand
+            </h1>
+            <p className="text-xl text-white">Find And Sell Real State</p>
+            <div className="mt-10">
+              <Link
+                to="/login"
+                className="text-sm font-bold rounded-lg bg-blue-600 px-5 py-4 text-white"
+              >
+                Get Started <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
